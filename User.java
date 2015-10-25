10fee932558e1910881a2d799b48043daf33e372
BrunoEu <bruno.anjos96@hotmail.com>
2015-10-25 22:45:35 +0000

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public class User {
+
+	public String NAME;
+	
+	public User(Scanner in){
+		getName(in);
+	}
+	
+	public void getName(Scanner in){
+		System.out.print("NOME > ");
+		NAME = in.nextLine();
+	}
+	
+}
