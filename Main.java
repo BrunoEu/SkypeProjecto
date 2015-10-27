ca82829a6d389f4f9cfb21b943768749c7055101
MrLipdx <nomeaocalhas3@gmail.com>
2015-10-27 21:02:49 +0000

    Update Main.java
    Criei o loop principal onde vai correr o interpretador de comandos
    Criei metodo para ler comandos

--- Main.java
+++ Main.java
@@ -1,10 +1,35 @@
+package randoms;
+
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner in = new Scanner(System.in);
+		
 		Chat novaConversa = new Chat(in);
+		
+		String cmd ;
+		
+		boolean leave = false;
+		
+		//loop principal do main
+		while(!leave)
+		{
+			//ler comando
+			cmd = processCmd(in);
+			
+			switch (cmd){
+			}
+			
+		}
 	}
-
+	
+	public static String processCmd(Scanner in)
+	{
+		System.out.print("> ");
+		//ler o que foi introduzido passar a letra maiuscula e tirar espacos no inicio e fim da string
+		return in.nextLine().toUpperCase().trim();
+	}
 }
