b1f7123d08a5fbbf4fdcefc1577da638d58af773
BrunoEu <bruno.anjos96@hotmail.com>
2015-11-03 11:32:40 +0000

    Update User.java

--- User.java
+++ User.java
@@ -1,7 +1,20 @@
-/*****
- * 
- *A CLASS USER DEIXOU DE EXISTIR 
- *FOI TUDO IMPLEMENTADO NA CHAT
- *NAO FAZIA SENTIDO 
- * 
-******/
+
+public class User {
+
+	public String userName;
+	public int userNumber;
+	
+	public User(String name, int number){
+		userName = name;
+		userNumber = number;
+	}
+	
+	public String getName(){
+		return userName;
+	}
+	
+	public int getNumber(){
+		return userNumber;
+	}
+	
+}
