188dd9f5b43f468a1c51b9091bc166a926cc76cf
BrunoEu <bruno.anjos96@hotmail.com>
2015-11-05 16:06:04 +0000

    Create Log.java

---
+++ Log.java
@@ -0,0 +1,18 @@
+
+public class Log {
+
+	private String log;
+	
+	public Log(User user1, User user2){
+		log = "Utilizador 1: "+user1.getName()+"\nUtilizador 2: "+user2.getName()+"\n\n";
+	}
+	
+	public void addToLog(String conversation){
+		log = log.concat(conversation);
+	}
+	
+	public String showLog(){
+		return log;
+	}
+	
+}
