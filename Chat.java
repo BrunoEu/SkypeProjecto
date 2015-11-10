598a7d357e21f500a955bf6a70450ee5159678a3
BrunoEu <bruno.anjos96@hotmail.com>
2015-11-10 11:30:55 +0000

    Update Chat.java

--- Chat.java
+++ Chat.java
@@ -1,59 +1,152 @@
 public class Chat {
 	
+	/*** CONSTANTES ***/
+	
 	private static final int MAX_FACTOR = 26;
 	private static final int MIN_FACTOR = 0;
 	
+	/*****************/
+	
+	
+	/*** VARIÁVEIS DE INSTÂNCIA ***/
+	
 	private int factor;
-	public User user1, user2;
+	private User user1, user2;
 	private String log;
+	private Conversation currentConversation;
+
+	/*****************************/
+
 	
-	public Conversation currentConversation;
-	
+	/*** CONSTRUTOR ***
+	 * 
+	 * @param name1 Nome do utilizador 1
+	 * @param name2 Nome do utilizador 2
+	 * @param newFactor Factor de Transaçao
+	 */
+	 
 	public Chat(String name1, String name2, int newFactor){
 		user1 = new User(name1, 1);
 		user2 = new User(name2, 2);
 		factor = newFactor;
-		initLog();
+		log = initLog();
 		currentConversation = new Conversation(user1, user2);
 	}
+	
+	
+	/*** 
+	 * 
+	 * @return Devolve a string com a conversa atual
+	 */
 	
 	public String showChat(){
 		return currentConversation.showConversation();
 	}
 	
+	
+	/***
+	 * 
+	 * @return 	"Nao ha conversas anteriores" Se o log nao tiver conversas
+	 * 			log Se tiver conversas
+	 */
+	
 	public String showLog(){
-		return log;
+		if (log.equals(initLog()))
+			return "Nao ha conversas anteriores\n";
+		else
+			return log;
 	}
+	
+	
+	/***
+	 * 
+	 * @param user 	Numero do utilizador
+	 * @param msg 	Mensagem
+	 * 
+	 * @pre validUser(user)
+	 */
 	
 	public void addMsg(int user,String msg){
 		currentConversation.addMsg(intToUser(user), msg);
 	}
 	
+	
+	/***
+	 * 
+	 * @param user 	Numero do utilizador
+	 * @param msg 	Mensagem
+	 * 
+	 * @pre validUser(user)
+	 */
+	
 	public void addEncMsg(int user, String msg){
 		currentConversation.addEncMsg(intToUser(user), msg, factor);
 	}
 	
+	
+	/*** TERMINA A CONVERSA ***/
+	
 	public void closeConversation(){
 		String conversation = currentConversation.showConversation();
-		addToLog(conversation);
+		log = formatToLog(conversation);
 		currentConversation.reset();
 	}
+	
+	
+	/**
+	 * 
+	 * @param user Numero do utilizador
+	 * @pre validUser(user)
+	 * 
+	 * @return 	true Se pode editar mensagem
+	 *			false Se nao pode editar mensagem
+	 */
 	
 	public boolean canEditLastMessage(int user){
 		return currentConversation.canEditLastMessage(intToUser(user));
 	}
 	
+	
+	/***
+	 * 
+	 * @param user 	Numero do utilizador
+	 * @param msg 	Mensagem
+	 * 
+	 * @pre validUser(user)
+	 */
+	
 	public void editLastMessage(int user, String msg){
 		currentConversation.editLastMessage(intToUser(user), msg, factor);
 	}
+	
+	
+	/**
+	 * 
+	 * @return String da ultima mensagem da conversa atual
+	 */
 	
 	public String getLastMsg(){
 		return currentConversation.getLastMsg();
 	}
 	
+	
+	/**
+	 * 
+	 * @return Int numero da mensagem
+	 */
+	
 	public int getMsgNumber(){
 		return currentConversation.getMsgNumber();
 	}
+	
+	
+	/**
+	 * 
+	 * @param user Numero do utilizador
+	 * @pre validUser(user)
+	 * 
+	 * @return 
+	 */
 	
 	public boolean validUser(int user){
 		return (user == user1.getNumber() || user == user2.getNumber());
@@ -70,12 +163,12 @@ public class Chat {
 			return user2;
 	}
 	
-	public void initLog(){
-		log = "Utilizador 1: "+user1.getName()+"\nUtilizador 2: "+user2.getName()+"\n\n";
+	public String initLog(){
+		return "Utilizador 1: "+user1.getName()+"\nUtilizador 2: "+user2.getName();
 	}
 	
-	public void addToLog(String msg){
-		log = log.concat(msg);
+	public String formatToLog(String msg){
+		return log.concat("\n\n**** NOVA CONVERSA ****\n").concat(msg);
 	}
 	
 }
