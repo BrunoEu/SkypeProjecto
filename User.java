bd7442ac82734b02aaa39a0d6d5a8976fca6dc4f
BrunoEu <bruno.anjos96@hotmail.com>
2015-11-12 15:13:51 +0000

    Update User.java

--- User.java
+++ User.java
@@ -1,17 +1,28 @@
 
 public class User {
 
-	private String userName;
-	private int userNumber;
+	/*** VARIÁVEIS DE INSTÂNCIA ***/
+	
+	private String userName;	//Nome do utilizador
+	private int userNumber;		//Numero do utilizador
+	
+	
+	/*** CONSTRUTOR ***/
 	
 	public User(String name, int number){
 		userName = name;
 		userNumber = number;
 	}
 	
+	
+	/*** DEVOLVE O NOME DO UTILIZADOR ***/
+	
 	public String getName(){
 		return userName;
 	}
+	
+	
+	/*** DEVOLVE O NUMERO DO UTILIZADOR ***/
 	
 	public int getNumber(){
 		return userNumber;
