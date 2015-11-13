8f4fbb8f92c4d5472973ac793fb355052ec7af70
BrunoEu <bruno.anjos96@hotmail.com>
2015-11-13 10:55:19 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 	
-	/***************** CONSTANTES *****************/
+	/***************** constantes *****************/
 
 	private static final String SHOW_CHAT = "VCP";
 	private static final String PUBLISH_MSG = "PNM";
@@ -14,101 +14,48 @@ public class Main {
 	private static final String HELP = "A";
 	private static final String EXIT = "S";
 	
+	/*
+		Nota: o método trim() é chamado ao longo
+		da classe para retirar os espaços do inicio
+		e fim da String.
+	*/
 	
-	/********** MÉTODO MAIN **********/
-	
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		int factor;		//Factor de Translação
-		
-		String name1;	//Nome do utilizador 1
-		String name2;	//Nome do utilizador 2
-		String command;		//Comando introduzido pelo utilizador
-		
-		//Pede o nome do utilizadores, garantindo
-		//que o do segundo não é igual ao do primeiro
-		name1 = getUsername(1, in); 
-		do{
-			name2 = getUsername(2, in);
-			if (name2.trim().equalsIgnoreCase(name1.trim()))
-				System.out.println("Nome já em utilização. "
-				+ "Por favor introduza um nome diferente.");
-		}while(name2.trim().equalsIgnoreCase(name1.trim()));
-		
-		//Pede o factor de translação
-		factor = getFactor(in);
-		
-		//Cria um novo objecto Chat
-		Chat newChat = new Chat(name1, name2, factor);
-		
-		//Apresenta os comandos disponíveis
-		processHelp();
-		
-		//Menu principal que interpreta comandos do utilizador
-		do{
-			command = processCommand(in);
-			switch(command){
-				case SHOW_CHAT: processShowChat(newChat); break;
-				case PUBLISH_MSG: processPubMsg(newChat, in); break;
-				case PUBLISH_ENC: processPubEnc(newChat, in); break;
-				case CORRECT_MSG: processCorrectMsg(newChat, in); break;
-				case CLOSE_CHAT: processCloseChat(newChat); break;
-				case SHOW_LOG: processShowLog(newChat); break;
-				case HELP: processHelp(); break;
-				default: System.out.println("Opcao inexistente.");
-			}
-		}while(command != EXIT);
-		
-		System.out.println("Aplicacao terminada. Ate a proxima.");
-		//Final do programa
-		
-	}
-	
-	
-	/*** interpretador de comandos ***/
-
 	private static String processCommand(Scanner in){
 		System.out.print("> ");
-		//ler o que foi introduzido passar a letra maiuscula e 
-		//tirar espaços no inicio e fim da string
 		return in.nextLine().toUpperCase().trim();
 	}
 	
 	
-	/*** APRESENTAR CONVERSA EM PROGRESSO ***/
-	
 	private static void processShowChat(Chat newChat){
-		System.out.print(newChat.showChat());
+		if(newChat.showChat().isEmpty())
+			System.out.println("Conversa Vazia.");
+		else
+			System.out.print(newChat.showChat());
 	}
 	
 	
-	/*** PUBLICAR MENSAGEM ***/
+	private static void processPlainMsg(Chat newChat, Scanner in){
+		publishMsg(newChat, in, false);
+	}
 	
-	private static void processPubMsg(Chat newChat, Scanner in){
+	
+	private static void processEncryptedMsg(Chat newChat, Scanner in){
+		publishMsg(newChat, in, true);
+	}
+	
+
+	private static void publishMsg(Chat newChat, Scanner in, boolean encrypted){
 		if (newChat.showChat().isEmpty())
 			System.out.println("Nova conversa iniciada");
 		
 		int user = getUserNumber(newChat, in);
-		newChat.addMsg(user, getMsg(in));
-		System.out.println("USER[" + user + "]MSG[" + newChat.getMsgNumber()+"]: Publicada");
+		if(encrypted)
+			newChat.addEncMsg(user, getMsg(in));
+		else
+			newChat.addMsg(user, getMsg(in));
+		System.out.print(newChat.formatMessage(user, "Publicada"));
 	}
 	
-	
-	/*** PUBLICAR MENSAGEM ENCRIPTADA ***/
-	
-	private static void processPubEnc(Chat newChat, Scanner in){
-		if (newChat.showChat().isEmpty())
-			System.out.println("Nova conversa iniciada");
-		
-		int user = getUserNumber(newChat, in);
-		newChat.addEncMsg(user, getMsg(in));
-		System.out.println("USER[" + user + "]MSG[" + newChat.getMsgNumber()+"]: Publicada");
-	}
-	
-	
-	/*** CORRIGIR MENSAGEM ANTERIOR ***/
 	
 	private static void processCorrectMsg(Chat newChat, Scanner in){
 		if(newChat.showChat().isEmpty())
@@ -126,23 +73,19 @@ public class Main {
 	}
 	
 	
-	/*** ENCERRAR CONVERSA EM PROGRESSO ***/
-	
 	private static void processCloseChat(Chat newChat){
 		newChat.closeConversation();
 		System.out.println("Conversa terminada.");
 	}
 
 	
-
-	/*** APRESENTAR HISTORICO DE CONVERSAS ***/
-	
 	private static void processShowLog(Chat newChat) {
-		System.out.print(newChat.showLog());
+		if (newChat.showLog().equals(newChat.initLog()))
+			System.out.println("Nao ha conversas anteriores");
+		else
+			System.out.print(newChat.showLog());
 	}
-	
 
-	/*** APRESENTAR COMANDOS DISPONIVEIS ***/
 	
 	private static void processHelp() {
 		System.out.println("\nVCP - Ver a conversa em progresso\n"+
@@ -156,16 +99,12 @@ public class Main {
 	}
 	
 	
-	/*** PEDIR MENSAGEM ***/
-	
 	private static String getMsg(Scanner in){
 		System.out.print("Mensagem: ");
 		String msg = in.nextLine();
 		return msg;
 	}
 	
-	
-	/*** PEDIR NUMERO DE UTILIZADOR ***/
 	
 	private static int getUserNumber(Chat newChat, Scanner in){
 		int user;
@@ -180,15 +119,11 @@ public class Main {
 	}
 
 	
-	/*** PEDIR NOME DE UTILIZADOR ***/
-	
 	private static String getUsername(int number, Scanner in){
 		System.out.print("Nome do Utilizador "+number+": ");
 		return in.nextLine();
 	}
 	
-	
-	/*** PEDIR FACTOR DE ENCRIPTAÇÃO ***/
 	
 	private static int getFactor(Scanner in){
 		int factor;
@@ -202,6 +137,51 @@ public class Main {
 		}while(!Chat.validFactor(factor));
 		
 		return factor;
+	}
+
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int factor;
+		
+		String name1;
+		String name2;
+		String command;
+	
+		name1 = getUsername(1, in);
+		
+		do{
+			name2 = getUsername(2, in);
+			if (name2.trim().equalsIgnoreCase(name1.trim()))
+				System.out.println("Nome já em utilização. "
+				+ "Por favor introduza um nome diferente.");
+		}while(name2.trim().equalsIgnoreCase(name1.trim()));
+		
+		factor = getFactor(in);
+		
+		Chat newChat = new Chat(name1, name2, factor);
+		
+		processHelp();
+		
+		do{
+			command = processCommand(in);
+			switch(command){
+				case SHOW_CHAT: processShowChat(newChat); break;
+				case PUBLISH_MSG: processPlainMsg(newChat, in); break;
+				case PUBLISH_ENC: processEncryptedMsg(newChat, in); break;
+				case CORRECT_MSG: processCorrectMsg(newChat, in); break;
+				case CLOSE_CHAT: processCloseChat(newChat); break;
+				case SHOW_LOG: processShowLog(newChat); break;
+				case HELP: processHelp(); break;
+				default: System.out.println("Opcao inexistente.");
+			}
+		}while(command != EXIT);
+		
+		System.out.println("Aplicacao terminada. Ate a proxima.");
+	
+		in.close();
 	}
 	
 	
